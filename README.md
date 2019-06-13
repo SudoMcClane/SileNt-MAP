2053fca477e9b04def5a4a294bdcb03c462209fd
Clément GATEFAIT <clement.gatefait@bt.com>
2019-06-13 17:19:03 +0200

    v0.90 Added multithreading feature

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NMAP LAUNCHER
 
 Launch multiple port scans on multiple IP ranges one after another
+Script also allows to launch scans in threads from multiple IP and
+MAC source addresses for smaller network footprint.
 
 ## Purpose
 
@@ -8,22 +10,27 @@ We needed to launch scans for open ports on multiple IP ranges without
 multithreading. To do so we first scanned most common ports on every IP range
 and then increased the ports list.
 
+Now the project has evolved to add multiple source addresses for scans to
+prevent network detection when performing Red Teaming for example.
+
 ## Prerequisites
 
 * [python3](https://www.python.org/downloads/)
 * [termcolor](https://pypi.org/project/termcolor/)
+* [netifaces](https://pypi.org/project/netifaces/)
 
 ## Help
 
 ```
 usage: nmap_launcher.py [-h] [-i FILENAME] [-p FILENAME] [-n NUMBER]
-                        [-d DELAY] [-v] [-f STRING] [-o DIRECTORY]
-                        {oX,oN,oG,oA,oS} ...
+                        [-d DELAY] [-v] [-f STRING] [-o DIRECTORY] [-e DEVICE]
+                        [--spoof-ip FILENAME] [--spoof-mac FILENAME]
+                        {oS,oN,oX,oG,oA} ...
 
 Launch multiple port scans on multiple IP ranges one after another
 
 positional arguments:
-  {oX,oN,oG,oA,oS}      Output type (See Nmap manpage)
+  {oS,oN,oX,oG,oA}      Output type (See Nmap manpage)
   additional            Additional arguments for nmap
 
 optional arguments:
@@ -32,7 +39,7 @@ optional arguments:
                         File with IPs/IP ranges to scan (one per line)
   -p FILENAME, --ports FILENAME
                         File with port list to scan (one per line: '80',
-			'T:443' or '8000-8010')
+                        'T:443' or '8000-8010')
   -n NUMBER, --number NUMBER
                         Number of Ports/Port ranges to scan at the same time
   -d DELAY, --delay DELAY
@@ -42,4 +49,10 @@ optional arguments:
                         Output filenames format (STRING °/. (ip, port))
   -o DIRECTORY, --output DIRECTORY
                         Directory name for scan results
+  -e DEVICE, --dev DEVICE
+                        Network interface to use
+  --spoof-ip FILENAME   File containing IP addresses used as source for Nmap
+                        (one per line). Retrieved using DHCP if empty
+  --spoof-mac FILENAME  File containing MAC addresses used as source for Nmap
+                        (one per line)
 ```
--- launcher.py
+++ launcher.py
@@ -3,84 +3,370 @@
 
 import argparse
 from termcolor import colored
+import netifaces
 import os
+import queue
+import socket
 import subprocess
+import sys
+import threading
 import time
 
-# Parsing arguments
-parser = argparse.ArgumentParser(prog="nmap_launcher.py",
-        description="Launch multiple port scans on multiple IP ranges one after another")
+def ParseArgs():
 
-parser.add_argument("-i", "--ips", metavar="FILENAME",
-        help="File with IPs/IP ranges to scan (one per line)")
-parser.add_argument("-p", "--ports", metavar="FILENAME",
-        help="File with port list to scan (one per line: '80', 'T:443' or '8000-8010')")
-parser.add_argument("-n", "--number", default=5, type=int,
-        help="Number of Ports/Port ranges to scan at the same time")
-parser.add_argument("-d", "--delay", default=0, type=int,
-        help="Pause between two scans (in seconds)")
-parser.add_argument("-v", "--verbose", default=False, action="store_true",
-        help="Show additional messages")
-parser.add_argument("type", choices={'oN', 'oX', 'oS', 'oG', 'oA'}, default='oA',
-        help="Output type (See Nmap manpage)")
-parser.add_argument("-f", "--format", metavar="STRING", default="%s-p%s",
-        help='Output filenames format (STRING °/. (ip, port))')
-parser.add_argument("-o", "--output", metavar="DIRECTORY", default="nmap_results",
-        help="Directory name for scan results")
-parser.add_argument("additional", nargs=argparse.REMAINDER,
-        help="Additional arguments for nmap")
+    # Parsing arguments
+    parser = argparse.ArgumentParser(prog="nmap_launcher.py",
+            description="Launch multiple port scans on multiple IP ranges one after another")
 
-args = parser.parse_args()
+    parser.add_argument("-i", "--ips", metavar="FILENAME",
+            help="File with IPs/IP ranges to scan (one per line)")
+    parser.add_argument("-p", "--ports", metavar="FILENAME",
+            help="File with port list to scan (one per line: '80', 'T:443' or '8000-8010')")
+    parser.add_argument("-n", "--number", default=5, type=int,
+            help="Number of Ports/Port ranges to scan at the same time")
+    parser.add_argument("-d", "--delay", default=0, type=int,
+            help="Pause between two scans (in seconds)")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true",
+            help="Show additional messages")
+    parser.add_argument("type", choices={'oN', 'oX', 'oS', 'oG', 'oA'}, default='oA',
+            help="Output type (See Nmap manpage)")
+    parser.add_argument("-f", "--format", metavar="STRING", default="%s-p%s",
+            help='Output filenames format (STRING °/. (ip, port))')
+    parser.add_argument("-o", "--output", metavar="DIRECTORY", default="nmap_results",
+            help="Directory name for scan results")
+    parser.add_argument("-e", "--dev", metavar="DEVICE", default="eth0",
+            help="Network interface to use")
+    parser.add_argument("--spoof-ip", metavar="FILENAME",
+            help="File containing IP addresses used as source for Nmap (one per line). \
+                    Retrieved using DHCP if empty")
+    parser.add_argument("--spoof-mac", metavar="FILENAME",
+            help="File containing MAC addresses used as source for Nmap (one per line)")
+    parser.add_argument("additional", nargs=argparse.REMAINDER, \
+            help="Additional arguments for nmap")
 
-# Check that IPs and Ports lists exist and ask for the filenames if needed
-while args.ips is None or not os.path.exists(args.ips):
-    args.ips = raw_input("Enter the path of the file with IPs/IP ranges to scan: ")
+    args = parser.parse_args()
 
-while args.ports is None or not os.path.exists(args.ports):
-    args.ports = raw_input("Enter the path of the file with Ports/Port ranges to scan: ")
+    # Check that spoof-ip and spoof-file are specified or None
+    if (args.spoof_ip is None) != (args.spoof_mac is None):
+        print(colored("[-] Fatal error: --spoof-ip and --spof-mac options must be used together", \
+                'red'))
+        parser.print_usage()
+        exit(1)
 
-# Create the output directory
-if not os.path.exists(args.output):
-    if args.verbose:
-        print(colored("[i] Creating %s directory", 'yellow') % args.output)
-    os.makedirs(args.output)
+    # Check that IPs and Ports lists exist and ask for the filenames if needed
+    while args.ips is None or not os.path.exists(args.ips):
+        args.ips = input("Enter the path of the file with IPs/IP ranges to scan: ")
 
-portfile = open(args.ports, "r")
+    while args.ports is None or not os.path.exists(args.ports):
+        args.ports = input("Enter the path of the file with Ports/Port ranges to scan: ")
 
-portrange = portfile.readline().replace(os.linesep, "")
-while portrange != "":                      # Until the is no port left
-    for i in range(1, int(args.number)):    # Concatenate ports ex: "80,443,8010-8020"
-        line = portfile.readline()
-        if line == "":
-            break
-        portrange += "," + line.replace(os.linesep, "")
-    if args.verbose:
-        print(colored("[i] Starting scanning ports %s", 'yellow') % portrange)
+    return args
 
-    ipfile = open(args.ips, "r")
-    for iprange in ipfile:                  # Until every IP has been scanned
-        iprange = iprange.replace(os.linesep, "")
 
-        # Call Nmap
-        command = ['nmap', iprange, '-p', portrange, "-" + args.type,
-                os.path.join(args.output, args.format % (iprange.replace("/", "-"),
-                    portrange.replace("-", "--").replace(",", "-")))] + args.additional
-                
-        if args.verbose:
-            print(colored("[+] Starting scanning %s:%s", 'yellow') % (iprange, portrange))
-            print(colored(' '.join(command), 'white', 'on_blue'))
+# Retrieves IPs either in the provided file, or using DHCP
+def GetIPs(devName, spoofMACFile, spoofIPFile, verbose):
+
+    ips = []
+
+    prevIPs = []
+    for line in spoofIPFile:
+        prevIPs.append(line.rstrip(os.linesep))
+
+    for macAddress in spoofMACFile:
+
+        # Remove trailing cariage return
+        macAddress = macAddress.rstrip(os.linesep)
+            
+        # Verbose output
+        if verbose:
+            print(colored("[i] MAC retrieved from file: %s", 'yellow') % macAddress)
+
+        if prevIPs:
+
+            ip = prevIPs.pop(0).rstrip(os.linesep)
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] IP retrieved from file: %s", 'yellow') % ip)
+
+        else:
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] Setting %s down", 'yellow') % devName)
+
+            # ip link set down dev eth0
+            subprocess.call(["ip", "link", "set", "down", "dev", devName])
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] Setting %s as MAC address", 'yellow') % macAddress)
+
+            # ip link set dev eth0 address AA:BB:CC:DD:EE:FF
+            subprocess.call(["ip", "link", "set", "dev", devName, "address", macAddress])
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] Setting %s up", 'yellow') % devName)
+
+            # ip link set up dev eth0
+            subprocess.call(["ip", "link", "set", "up", "dev", devName])
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] Performing DHCP request", 'yellow'))
+
+            # dhclient eth0
+            subprocess.call(["dhclient", devName])
+            # Retrieving IP address
+            ip = netifaces.ifaddresses(devName)[netifaces.AF_INET][0]['addr'] + "/" \
+                    + netifaces.ifaddresses(devName)[netifaces.AF_INET][0]['netmask']
+
+            # Save IPs in the file
+            spoofIPFile.write(ip + os.linesep)
+
+            # Verbose output
+            if verbose:
+                print(colored("[i] New IP retrieved: %s", 'yellow') % ip)
+
+        # Verbose output
+        if verbose:
+            print(colored("[+] IP - MAC couple: %s - %s", 'green') % (ip, macAddress))
+
+        ips.append([macAddress, ip])
+
+    return ips
+
+
+# Nmap calling function
+def CallNmap(iprange, portrange, type, outDir, outFormat, args, verbose, \
+        iface=None, ip=None, mac=None):
+
+    # Output filename
+    outFile = os.path.join(outDir, outFormat % (iprange.replace("/", "-"),
+                portrange.replace("-", "--").replace(",", "-")))
+
+    # Call Nmap
+    if iface is None or ip is None:
+        command = ['nmap', iprange, '-p', portrange, "-" + type, outFile] + args
+    elif mac is None:
+        command = ['nmap', iprange, '-p', portrange, "-" + type, outFile, "-e", iface, "-S", \
+                ip.split('/')[0]] + args
+    else:
+        command = ['nmap', iprange, '-p', portrange, "-" + type, outFile, "-e", iface, "-S", \
+                ip.split('/')[0], "--spoof-mac", mac] + args
+
+    # Verbose output
+    if verbose:
+        print(colored("[+] Starting scanning %s:%s", 'yellow') % (iprange, portrange))
+        print(colored(' '.join(command), 'white', 'on_blue'))
+    
+    stdout = open(outFile + ".stdout", "wb")
+    stderr = open(outFile + ".stderr", "wb")
+    subprocess.call(command, stdout=stdout, stderr=stderr)
+    stdout.close()
+    stderr.close()
+
+    # Verbose output
+    if verbose:
+        print(colored("[+] %s:%s scanned", 'green') % (iprange, portrange))
+
+
+# Class for multithreaded scans
+class iFaceThread (threading.Thread):
+
+    def __init__(self, id, ip, scanQueue, lock, type, outDir, outFormat, args, verbose, iface, mac):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.ip = ip
+        self.scanQueue = scanQueue
+        self.lock = lock
+        self.type = type
+        self.outDir = outDir
+        self.outFormat = outFormat
+        self.args = args
+        self.verbose = verbose
+        self.iface = iface
+        self.mac = mac
+
+    def run(self):
+
+        # Create interface
+        dev = self.iface
         
-        subprocess.call(command)
+        # Quick & Dirty fix for -Pn & --spoof-mac incompatibility
+        if "-Pn" in self.args:
+            self.mac = None
+        
+        # Verbose output
+        if self.verbose:
+            print(colored("[i] Setting %s as MAC address", 'yellow') % self.mac)
 
+        # ip link set dev eth0 address AA:BB:CC:DD:EE:FF
+        subprocess.call(["ip", "address", "add", self.ip, "dev", dev])
+
+        while not self.scanQueue.empty():
+            #lock.acquire()
+            item = self.scanQueue.get()
+            #lock.release()
+
+            # Queue ends with "None"s
+            if item is None:
+                self.scanQueue.task_done()
+
+                # Verbose output
+                if self.verbose:
+                    print(colored("[i] Stopping thread for interface %s/%s", 'yellow') \
+                            % (self.ip, self.mac))
+
+                break
+
+            iprange = item[0]
+            portrange = item[1]
+
+            CallNmap(iprange, portrange, self.type, self.outDir, self.outFormat, self.args, \
+                    self.verbose, dev, self.ip, self.mac)
+
+            self.scanQueue.task_done()
+
+
+# Funcion creating ip/port couples to scan
+def createScanQueue(ipFile, portFile, verbose):
+
+    # Verbose output
+    if verbose:
+        print(colored("[i] Creating Scan Queue%s", 'yellow') % os.linesep)
+
+    # create lists
+    res = queue.Queue()
+    ips = []
+
+    # read files
+    for ipRange in ipFile:
+        ips.append(ipRange.strip(os.linesep))
+
+    for portRange in portFile:
+
+        # Remove \newline
+        portRange = portRange.strip(os.linesep)
+
+        for ipRange in ips:
+            res.put([ipRange, portRange])
+
+            # Verbose output
+            if verbose:
+                print(colored("  Ip(s):\t%s%s  Port(s):\t%s%s", 'yellow') \
+                        % (ipRange, os.linesep, portRange, os.linesep))
+
+    # Verbose output
+    if verbose:
+        print(colored("[i] Scan Queue created", 'yellow'))
+
+    return res
+
+
+# Create and launch Threads
+def launchThreads(ipFile, portFile, spoofIPFile, spoofMACFile, dev, type, outDir, outFormat, \
+        args, verbose):
+    
+    # Backup IP/MAC
+    ipBack = netifaces.ifaddresses(dev)[netifaces.AF_INET][0]['addr'] + "/" \
+            + netifaces.ifaddresses(dev)[netifaces.AF_INET][0]['netmask']
+    macBack = netifaces.ifaddresses(dev)[netifaces.AF_LINK][0]['addr']
+
+    # retrieve source IPs
+    ips = GetIPs(dev, spoofMACFile, spoofIPFile, verbose)
+
+    queue = createScanQueue(ipFile, portFile, verbose)
+
+    threads = []
+    i = 0
+    for couple in ips:
+        t = iFaceThread(i, couple[1], queue, None, type, outDir, outFormat, args, verbose, dev, \
+                couple[0])
+        i = i + 1
+        threads.append(t)
+        t.start()
+        # Add 'None' at the end of the queue to stop the thread
+        queue.put(None)
+
+    # Wait for threads
+    queue.join()
+    
+    # Restore IP/MAC backup
+    subprocess.call(["ip", "link", "set", "down", "dev", dev])
+    subprocess.call(["ip", "link", "set", "dev", dev, "address", macBack])
+    subprocess.call(["ip", "address", "add", ipBack, "dev", dev])
+    subprocess.call(["ip", "link", "set", "up", "dev", dev])
+
+
+# Main function
+def Main():
+
+    args = ParseArgs()
+
+    # Create the output directory
+    if not os.path.exists(args.output):
+
+        # Verbose output
         if args.verbose:
-            print(colored("[+] %s:%s scanned", 'green') % (iprange, portrange))
+            print(colored("[i] Creating %s directory", 'yellow') % args.output)
+        os.makedirs(args.output)
 
-        time.sleep(int(args.delay))
-    ipfile.close()
+    # Case multithread
+    if args.spoof_ip:
+        portFile = open(args.ports, "r")            # already checked in ParseArgs()
+        ipFile = open(args.ips, "r")
+        if not os.path.isfile(args.spoof_ip):
+            os.mknod(args.spoof_ip)
+        spoofIPFile = open(args.spoof_ip, "r+")
+        spoofMacFile = open(args.spoof_mac, "r")
 
-    portrange = portfile.readline().replace(os.linesep, "")
+        launchThreads(ipFile, portFile, spoofIPFile, spoofMacFile, args.dev, args.type, \
+                args.output, args.format, args.additional, args.verbose)
 
-portfile.close()
+        # Cleaning
+        portFile.close()
+        ipFile.close()
+        spoofIPFile.close()
+        spoofMacFile.close()
 
-if args.verbose:
-    print(colored("[+] Scan finished", 'green'))
+    # Case not threaded
+    else:
+        portFile = open(args.ports, "r")            # already checked in ParseArgs()
+
+        portrange = portFile.readline().rstrip(os.linesep)
+        while portrange != "":                      # Until there is no port left
+            for i in range(1, int(args.number)):    # Concatenate ports ex: "80,443,8010-8020"
+                line = portFile.readline()
+                if line == "":
+                    break
+                portrange += "," + line.rstrip(os.linesep)
+
+            # Verbose output
+            if args.verbose:
+                print(colored("[i] Starting scanning ports %s", 'yellow') % portrange)
+
+            ipFile = open(args.ips, "r")
+            for iprange in ipFile:                  # Until every IP has been scanned
+                iprange = iprange.replace(os.linesep, "")
+
+                # Call Nmap
+                CallNmap(iprange, portrange, args.type, args.output, args.format, args.additional, \
+                        args.verbose)
+
+                time.sleep(int(args.delay))
+            ipFile.close()
+
+            portrange = portFile.readline().replace(os.linesep, "")
+
+        portFile.close()
+
+    # Verbose output
+    if args.verbose:
+        print(colored("[+] Scan finished", 'green'))
+
+    sys.exit(0)
+
+
+Main()
